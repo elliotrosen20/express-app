6c159f70b8a62b590503124dc195c4c34a89ec60
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-17 17:11:10 -0400

    Add files w/ routing and mvc system incorporated.

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", { message: "EJS rocks!" });
+});
+
+app.use("/authors", authorRouter)
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+    console.error(err);
+    // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+    res.status(err.statusCode || 500).send(err.message);
+});
+  
+
+  
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
